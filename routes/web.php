ab2173a6cd3dd2fc5eaf3317dac0d9758c98b522
socramreis <jmreis.ti@gmail.com>
2023-01-18 21:13:29 -0300

    Editar Client

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::prefix('client')->group(function()
     Route::get('list' , [ClientController::class , 'list'])->name('client-list');
     Route::get('create' , [ClientController::class , 'create'])->name('client-create');
     Route::post('/' , [ClientController::class , 'store'])->name('client-store');
+    Route::post('/{idclient}/edit' , [ClientController::class , 'edit'])->name('client-edit');
+
 
 }) ;
 
